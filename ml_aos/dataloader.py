fbac27e0d7c2af1e77ea05eb28ec618cfbac0acf
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2022-01-14 20:59:51 -0800

    Fixed errors, optimized code.

--- ml_aos/dataloader.py
+++ ml_aos/dataloader.py
@@ -37,8 +37,8 @@ class Donuts(Dataset):
         dither: bool = True,
         mask_blends: bool = False,
         fix_seed: bool = True,
-        nval: int = 256,
-        ntest: int = 2048,
+        nval: int = 2 ** 16,
+        ntest: int = 2 ** 16,
     ):
         """Load the simulated AOS donuts and zernikes in a Pytorch Dataset.
 
@@ -76,10 +76,6 @@ class Donuts(Dataset):
             "fix_seed": fix_seed,
         }
 
-        # load the metadata for the unblended and blended images
-        self.unblended_df = pd.read_csv(self.DATA_DIR + "unblended/record.csv")
-        self.blended_df = pd.read_csv(self.DATA_DIR + "blended/record.csv")
-
         # load the sky simulations
         self.sky = np.loadtxt(self.DATA_DIR + "sky.csv")
 
@@ -92,34 +88,42 @@ class Donuts(Dataset):
             self.N_UNBLENDED + self.N_BLENDED, nval + ntest, replace=False
         )
 
-        # save the index corresponding to the requested mode
+        # get the indices corresponding to the requested mode
         self.mode = mode
         if mode == "train":
-            self.len = Donuts.N_UNBLENDED + Donuts.N_BLENDED - nval - ntest
-            train = set(np.arange(self.len)) - set(holdout)
-            self.index = np.array(list(train))  # type: npt.NDArray[np.int64]
+            train = set(np.arange(Donuts.N_UNBLENDED + Donuts.N_BLENDED))
+            train -= set(holdout)
+            index = np.array(list(train))  # type: npt.NDArray[np.int64]
         elif mode == "val":
-            self.len = nval
-            self.index = holdout[:nval]
+            index = holdout[:nval]
         else:
-            self.len = ntest
-            self.index = holdout[nval:]
+            index = holdout[nval:]
 
-    def __getitem__(self, index: int) -> dict[str, torch.Tensor]:
+        # load metadata for the unblended images
+        unblended_df = pd.read_csv(self.DATA_DIR + "unblended/record.csv")
+        unblended_df = unblended_df.iloc[index[index < self.N_UNBLENDED]]
+        self.unblended_df = unblended_df.reset_index(drop=True)
+
+        # load metadata for the unblended images
+        blended_df = pd.read_csv(self.DATA_DIR + "blended/record.csv")
+        blended_df = blended_df.set_index(blended_df["idx"] + self.N_UNBLENDED)
+        self.blended_df = blended_df.loc[index[index >= self.N_UNBLENDED]]
+
+    def __len__(self) -> int:
+        return len(self.unblended_df) + len(self.blended_df)
+
+    def __getitem__(self, idx: int) -> dict[str, torch.Tensor]:
         """Return simulation corresponding to the index."""
-        if index < 0 or index > self.len:
-            raise ValueError("index out of bounds.")
+        if idx < 0 or idx > len(self):
+            raise ValueError("idx out of bounds.")
 
         # get the simulations for this index
-        idx = self.index[index]
         if idx < self.N_UNBLENDED:
             blend_flag = False
-            img, fx, fy, intra, zernikes = self._get_unblended(idx)
+            img, fx, fy, px, py, intra, zernikes = self._get_unblended(idx)
         else:
             blend_flag = True
-            img, fx, fy, intra, zernikes = self._get_blended(
-                idx - self.N_UNBLENDED
-            )
+            img, fx, fy, px, py, intra, zernikes = self._get_blended(idx)
 
         # apply image distortions
         settings = self.settings
@@ -135,6 +139,8 @@ class Donuts(Dataset):
             "image": torch.from_numpy(img).reshape(1, 256, 256),
             "field_x": torch.FloatTensor([fx]),
             "field_y": torch.FloatTensor([fy]),
+            "focal_x": torch.FloatTensor([px]),
+            "focal_y": torch.FloatTensor([px]),
             "intrafocal": torch.FloatTensor([intra]),
             "zernikes": torch.from_numpy(zernikes),
             "blended": torch.ByteTensor([blend_flag]),
@@ -148,66 +154,75 @@ class Donuts(Dataset):
         blend_idx: int = None,
     ) -> tuple[
         npt.NDArray[np.float64],
-        npt.NDArray[np.float64],
+        np.float64,
+        np.float64,
         np.float64,
         np.float64,
         bool,
+        npt.NDArray[np.float64],
     ]:
         """Return an unblended simulation."""
+
         if blend_idx is None:
             img_file = self.DATA_DIR + f"unblended/{idx}.image"
             zernike_file = self.DATA_DIR + f"unblended/{idx}.zernike"
             metadata = self.unblended_df.iloc[idx]
         else:
-            img_file = self.DATA_DIR + f"blended/{idx}_{blend_idx}.image"
-            zernike_file = self.DATA_DIR + f"blended/{idx}_0.zernike"
-            metadata = self.blended_df.query(
-                f"idx == {idx} & neighborID == {blend_idx}"
-            )
+            file_idx = idx - self.N_UNBLENDED
+            img_file = self.DATA_DIR + f"blended/{file_idx}_{blend_idx}.image"
+            zernike_file = self.DATA_DIR + f"blended/{file_idx}_0.zernike"
+            metadata = self.blended_df.loc[idx].iloc[blend_idx]
 
         # load the image array
         img = np.load(img_file)
 
-        # load the zernike coefficients; we use 4-21, inclusive
-        zernikes = np.load(zernike_file)[4:22]
-
         # load the field position, in radians
         fx, fy = metadata.fieldx, metadata.fieldy
+
+        # load the focal plane position, in radians
+        px, py = metadata.posx, metadata.posy
 
         # boolean stating whether the image is intrafocal
         # obviously, if false, the image is extrafocal
         intra = "SW0" in metadata.chip
 
-        return img, zernikes, fx, fy, intra
+        # load the zernike coefficients; we use 4-21, inclusive
+        zernikes = np.load(zernike_file)[4:22]
+
+        return img, fx, fy, px, py, intra, zernikes
 
     def _get_blended(
         self,
         idx: int,
     ) -> tuple[
         npt.NDArray[np.float64],
-        npt.NDArray[np.float64],
+        np.float64,
+        np.float64,
         np.float64,
         np.float64,
         bool,
+        npt.NDArray[np.float64],
     ]:
         """Return a blended simulation."""
         # get the central star
-        img, zernikes, fx, fy, intra = self._get_unblended(idx, blend_idx=0)
+        img, fx, fy, px, py, intra, zernikes = self._get_unblended(
+            idx, blend_idx=0
+        )
 
         # now loop over the neighbors
-        neighbors = self.blended_df.query(f"idx == {idx}").neighborId[1:]
+        neighbors = self.blended_df.loc[idx].neighborId[1:]
         for n in neighbors:
             # get the neighbor, unblended
-            n_img, _, n_fx, n_fy, _ = self._get_unblended(idx, blend_idx=n)
+            n_img, _, _, n_px, n_py, *_ = self._get_unblended(idx, blend_idx=n)
 
             # if we're masking the blends, set all positive pixels to NaN
             # later we will cast all NaNs to zero
             if self.settings["mask_blends"]:
-                n_img = np.where(n_img > 0, np.nan, 0)
+                n_img = np.where(n_img > 10, np.nan, n_img)
 
             # get distance in pixels
-            dx = round((fx - n_fx) * self.PIXELS_PER_RADIAN)
-            dy = round((fy - n_fy) * self.PIXELS_PER_RADIAN)
+            dx = round((px - n_px) * self.PIXELS_PER_RADIAN)
+            dy = round((py - n_py) * self.PIXELS_PER_RADIAN)
 
             # get slices of central and neighbor images
             img_xslice = slice(max(0, dx), min(256, 256 + dx))
@@ -217,4 +232,4 @@ class Donuts(Dataset):
 
             img[img_yslice, img_xslice] += n_img[n_img_yslice, n_img_xslice]
 
-        return img, zernikes, fx, fy, intra
+        return img, fx, fy, px, py, intra, zernikes
